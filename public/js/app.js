e8e7f872c28977408f9c11d74b91781de8a1e924
Suhyun <aodlfduddj@gmail.com>
2020-06-10 22:24:30 +0900

    Add about text

--- public/js/app.js
+++ public/js/app.js
@@ -13,6 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
+                messageTwo.textContent = ''
             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
